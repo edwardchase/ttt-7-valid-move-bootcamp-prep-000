364bcdcd0ab8a27607834ea311022f99fdff4d97
Edward Mixon <edwardchase@gatech.edu>
2018-06-09 16:41:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 def valid_move? (board, index)
   if index == 0 ||index == 1 ||index ==2 ||index == 3 ||index == 4 ||index == 5 ||index == 6 ||index == 7 ||index == 8
     true 
+    elsif board[index] != "" || board[index] != " "
+    false 
   end 
 end
 
