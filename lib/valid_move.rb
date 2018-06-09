8e55fbbadb01419f52acce0eaa6f50e0d2ed1046
Edward Mixon <edwardchase@gatech.edu>
2018-06-09 16:36:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,4 @@
-# code your #valid_move? method here
-
+def valid_move? (board, index)
+  if index == [1,2,3,4,5,6,7,8,9]
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
